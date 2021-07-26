f2e1592dadce840d07e786806d1c40174dfb0f19
Matheus Henrique Dos Santos Lima <1995.matheus.lima@gmail.com>
2021-07-25 22:09:28 -0300

    Finished the chapter3  chapter3 from the book!

---
+++ Chapter3/3_1booleanExpressions.py
@@ -0,0 +1,18 @@
+# the operator == is used to compare two values and return true or false.
+#retorn True:
+x = 5 == 5
+print(x)
+#retorn False:
+y = 5 == 6
+print(y)
+
+# the operator == is one of the comparision operators, the others are:
+#  x != y       x is not equal to y
+#  x > y        x is greater than y
+#  x >= y       x is greater than or equal to y
+#  x < y        x is less than y
+#  x <= y       x is less than or equal to y
+#  x is y       x is the same as y
+#  x is not y   x is not the same as y
+
+ 
